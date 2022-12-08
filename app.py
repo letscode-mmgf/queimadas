5a370a8e853f05f8f24ea4aff5f7d3857f0ee4c2
Marcelo Mesquita <mm.marcelomesquita@gmail.com>
2022-12-08 19:23:11 -0300

    Inicio da modularização do app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
 from PIL import Image
 from tratamento import dataset
-from funcoes import personalizacao
+from funcoes import *
 
 st.set_page_config( layout='wide' )
 
@@ -19,21 +18,35 @@ analise = st.sidebar.selectbox(
     label="Tipo de Análise", 
     options=['Análise Univariada', 'Análise Bivariada', 'Análise Multivariada'])
 
+if analise == 'Análise Univariada': n = 1
+elif analise == 'Análise Bivariada': n = 2
+elif analise == 'Análise Multivariada': n = 3
+
 tipo = st.sidebar.radio(label='Tipo', options=['Linhas', 'Barras', 'Dispersão'], index=0)
 
-anos = st.sidebar.select_slider(label='Anos', options=(list(range(1998, 2018, 1))))
-
-regiao = st.sidebar.multiselect(
-    label="Região", 
-    options=['Norte', 'Nordeste', 'Sudeste', 'Sul', 'Centro Oeste', 'Centro']
+criterios = st.sidebar.multiselect(
+        label="Critérios", 
+        options=['Anos', 'Regiões', 'Estações'],
+        max_selections=n
 )
 
-estacoes = st.sidebar.multiselect(
-    label='Estações', 
-    options=['Outono', 'Inverno', 'Primavera', 'Verão']
-)
+if 'Estações' in criterios:
+    estacoes = st.sidebar.multiselect(
+        label='Estações', 
+        options=['Outono', 'Inverno', 'Primavera', 'Verão'],
+        default=['Outono', 'Inverno', 'Primavera', 'Verão']
+    )
 
+if 'Regiões' in criterios:
+    regioes = st.sidebar.multiselect(
+        label="Região", 
+        options=['Norte', 'Nordeste', 'Sudeste', 'Sul', 'Centro Oeste', 'Centro'],
+        default=['Norte', 'Nordeste', 'Sudeste', 'Sul', 'Centro Oeste', 'Centro']
+    )
 
+if 'Anos' in criterios:
+    anos = st.sidebar.select_slider(label='Anos', options=(list(range(1998, 2018, 1))))
+    
 st.title('Descrição dos dados')
 st.write('Os dados forem obtidos de uma fonte primária, abrangendo ocorrências de queimadas florestais no período de 1998 a 2017.')
 st.write('Os dados estão disponíveis através do kaggle pela URL abaixo:')
@@ -42,23 +55,26 @@ st.write('https://www.kaggle.com/datasets/gustavomodelli/forest-fires-in-brazil'
 
 st.write('Uma breve análise exploratória revela a natureza dos nossos dados:')
 
-# ---- modularizar essa parte -----
 df = dataset()
-columns=('col %d' % i for i in range(20))
-
-st.dataframe(df)  # deixar somente isso
+st.dataframe(df.head())
 
 st.write('Gráfico')
 
-
-fig = px.histogram(data_frame=df,
-                    x='state', 
-                    y='number',
-                    color='season', 
-                    barmode='group', 
-                    histfunc='avg')
-personalizacao(fig, 'Estados', 'Média de Queimadas', 'Estações')
-st.plotly_chart(fig)
+""" 
 
 
-rotulos = st.checkbox(label='Mostrar rótulos', value=False)
+#st.table(df.info())
+#descobrir como imprimir um info no streamlit
+
+
+st.write(regioes)
+filtro_regiao = []
+
+st.write(teste(regioes))
+
+if analise == 'Análise Univariada' and tipo == 'Linhas':
+    st.plotly_chart(get_line_chart(df))
+
+st.plotly_chart(get_histogram(df, filtro_regiao))
+
+rotulos = st.checkbox(label='Mostrar rótulos', value=False) """
--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,5 @@
+import plotly.express as px
+
 def personalizacao(variavel: vars, tit_x: str, tit_y: str, tit_leg: str):
     """Personalização
 
@@ -28,4 +30,33 @@ def personalizacao(variavel: vars, tit_x: str, tit_y: str, tit_leg: str):
                 
                 template='plotly_dark')
 
+def get_histogram(df, regioes):
 
+    if len(regioes) == 0: df_filtrado = df
+
+    if len(regioes) > 0:
+        filtro = (df['region'] in regioes)
+        df_filtrado = df[filtro]
+
+    fig = px.histogram(data_frame=df_filtrado,
+                        x='state', 
+                        y='number',
+                        color='season', 
+                        barmode='group', 
+                        histfunc='avg')
+
+    personalizacao(fig, 'Estados', 'Média de Queimadas', 'Estações')
+    return fig
+
+def get_line_chart(df):
+    df_plot = df.groupby('year')['number'].sum().reset_index().sort_values(by= 'year')
+    linha = px.line(data_frame=df_plot, x='year', y='number', markers=True, template='plotly_dark')
+    personalizacao(linha, 'Anos', 'Número de Queimadas Registradas', None)
+    return linha
+
+def teste(regioes):
+    filtro_regiao = []
+    for i, valor in enumerate(regioes):
+        filtro_regiao.append(valor)
+
+    return filtro_regiao
