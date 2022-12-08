2dc7fbbb21ca476031d95c4dcc1b332d4b18edfb
Guilherme Oliveira <silva.guilhermeoliveira@gmail.com>
2022-12-07 21:34:43 -0300

    chore: Att app.py

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ import streamlit as st
 st.title('Queimadas no Brasil')
 
 st.title('Introdução')
+st.write('Analisar as ocorrências de queimadas no Brasil, analisando critérios como regiões, meses, estações e evolução durante os anos')
 
 st.title('Descrição dos dados')
+st.write('Os dados forem obtidos de uma fonte primária, abrangendo ocorrências de queimadas florestais no período de 1998 a 2017.')
+st.write('Os dados estão disponíveis através do kaggle pela URL abaixo:')
+st.write('https://www.kaggle.com/datasets/gustavomodelli/forest-fires-in-brazil')
