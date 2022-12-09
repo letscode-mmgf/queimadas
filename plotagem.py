993db55db444c4bfc60e7cffdaf9f61ed51335e4
Guilherme Oliveira <silva.guilhermeoliveira@gmail.com>
2022-12-09 20:59:11 -0300

    fetch: Adcionando a função dispersion

--- plotagem.py
+++ plotagem.py
@@ -1,5 +1,6 @@
 import plotly.express as px
 
+
 def personalizacao(variavel: vars, tit_x: str, tit_y: str, tit_leg: str):
     """Personalização
 
@@ -13,54 +14,66 @@ def personalizacao(variavel: vars, tit_x: str, tit_y: str, tit_leg: str):
         Figure: Retorna o gráfico personalizado com titulos, padrões de cores e layout dark
     """
     return variavel.update_layout(
-                xaxis_title={
-                    'text': tit_x,
-                    'font_size': 12
-                },
-                yaxis_title={
-                    'text': tit_y,
-                    'font_size': 12
-                },
-                legend_title={
-                    'text': tit_leg,
-                    'font_size': 12
-                },
-                font_color='grey',
-                font_size=10,
-                
-                template='plotly_dark')
+        xaxis_title={
+            'text': tit_x,
+            'font_size': 12
+        },
+        yaxis_title={
+            'text': tit_y,
+            'font_size': 12
+        },
+        legend_title={
+            'text': tit_leg,
+            'font_size': 12
+        },
+        font_color='grey',
+        font_size=10,
+
+        template='plotly_dark')
+
 
 def get_line_chart(df):
-    df_plot = df.groupby('year')['number'].sum().reset_index().sort_values(by= 'year')
-    linha = px.line(data_frame=df_plot, x='year', y='number', markers=True, template='plotly_dark')
+    df_plot = df.groupby('year')['number'].sum(
+    ).reset_index().sort_values(by='year')
+    linha = px.line(data_frame=df_plot, x='year', y='number',
+                    markers=True, template='plotly_dark')
     personalizacao(linha, 'Anos', 'Número de Queimadas Registradas', None)
     return linha
+
 
 def get_histogram(df):
 
     histogram = px.histogram(data_frame=df,
-                        x='state', 
-                        y='number',
-                        color='season', 
-                        barmode='group', 
-                        histfunc='avg')
+                             x='state',
+                             y='number',
+                             color='season',
+                             barmode='group',
+                             histfunc='avg')
 
     personalizacao(histogram, 'Estados', 'Média de Queimadas', 'Estações')
     return histogram
 
 
 def get_dispersion(df):
-    df_plot = df.groupby('year')['number'].sum().reset_index().sort_values(by= 'year')
-    linha = px.scatter(data_frame=df_plot, x='year', y='number', markers=True, template='plotly_dark')
-    personalizacao(linha, 'Anos', 'Número de Queimadas Registradas', None)
-    return linha
+
+    grafico = px.scatter(data_frame=df,
+                         x='year',
+                         y='number',
+                         color='species')
+
+    personalizacao(grafico, 'Anos', 'Número de Queimadas Registradas',
+                   'Ano', 'Quantidade de ocorrência')
+    return grafico
+
 
 def get_line_chart_bivariada(df, **kwargs):
 
     colunas = list(kwargs.keys())
     df_agrupado = df.groupby(colunas)['number'].sum().reset_index()
-    grafico = px.line(data_frame=df_agrupado, x=colunas[0], y='number', color = colunas[1], markers=False, template='plotly_dark')
-    
-    personalizacao(grafico, 'Anos', 'Número de Queimadas Registradas', 'Regiões do Brasil')
+    grafico = px.line(data_frame=df_agrupado,
+                      x=colunas[0], y='number', color=colunas[1], markers=False, template='plotly_dark')
+
+    personalizacao(grafico, 'Anos',
+                   'Número de Queimadas Registradas', 'Regiões do Brasil')
 
     return grafico
